5ad18a3ba3fd34a6a154dac9df3d5c073f7274a5
ancotov <ancot@yqndex.ru>
2016-04-14 17:47:22 +0300

    333

---
+++ src/com/example/material_template/NavigationDrawerFragment.java
@@ -0,0 +1,24 @@
+package com.example.material_template;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class NavigationDrawerFragment extends Fragment {
+
+	public NavigationDrawerFragment() {
+		
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		
+		return inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
+		
+	}
+	
+	
+	
+}

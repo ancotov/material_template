c0f398afeb0cb338ac2f7ac3b93eefc697fc9719
ancotov <ancot@yqndex.ru>
2016-04-14 18:16:54 +0300

    last

--- src/com/example/material_template/NavigationDrawerFragment.java
+++ src/com/example/material_template/NavigationDrawerFragment.java
@@ -2,12 +2,18 @@ package com.example.material_template;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class NavigationDrawerFragment extends Fragment {
 
+	private ActionBarDrawerToggle mDrawerToggle;
+	private DrawerLayout mDrawerLayout;
+	
 	public NavigationDrawerFragment() {
 		
 	}
@@ -18,7 +24,28 @@ public class NavigationDrawerFragment extends Fragment {
 		return inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
 		
 	}
-	
+
+	public void setUp(DrawerLayout drawerLayout, Toolbar toolbar) {
+		
+		mDrawerLayout = drawerLayout;
+		mDrawerToggle = new ActionBarDrawerToggle(getActivity(), drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close){
+
+			@Override
+			public void onDrawerOpened(View drawerView) {
+				// TODO Auto-generated method stub
+				super.onDrawerOpened(drawerView);
+			}
+
+			@Override
+			public void onDrawerClosed(View drawerView) {
+				// TODO Auto-generated method stub
+				super.onDrawerClosed(drawerView);
+			}
+			
+		};
+		
+		mDrawerLayout.setDrawerListener(mDrawerToggle);
+	}
 	
 	
 }
